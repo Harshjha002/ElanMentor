992d76946213352f78f0e9ab6f53ab8eae7150b1
Harsh <harshjha8453@gmail.com>
2025-06-13 15:22:46 +0530

    feat(auth): add sign-in and sign-up forms with Zod validation and DB integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+"use client"
+import { authClient } from '@/lib/auth-client'
 import React from 'react'
 
 const HomePage = () => {
+   const { 
+        data: session,    
+    } = authClient.useSession() 
+
+    const username =session?.user.name 
+ 
+ 
+    if(session){
+    return <div>
+      <h1>
+        {username}
+      </h1>
+      <button onClick={() => authClient.signOut()}>
+        Log out
+      </button>
+    </div>
+  }
+
   return (
     <div>
       home page
